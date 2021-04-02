bf960c3a8fa3e237ef1c14c7c8a4929895c17992
Jay <jayrulz99@gmail.com>
2021-04-02 15:41:37 +0100

    commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///MyBooks.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
