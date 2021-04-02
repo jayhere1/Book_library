a6e206ea4ac3c997a368bf7d41a9394635ff5525
Jay <jayrulz99@gmail.com>
2021-04-02 16:02:09 +0100

    commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("sqlite:///MyBooks.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///MyBooks.db"
 
 # # os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
